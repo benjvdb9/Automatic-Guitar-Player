2fadf270f5d8d69fb32ef66d579c4ce45b92a82a
Benjamin Vandenbussche <benjvdb9@hotmail.com>
2019-02-15 13:34:39 +0100

    Created structure for right hand strumming

--- AGP.py
+++ AGP.py
@@ -9,6 +9,9 @@ class Arm(Thread):
     id = 0
     colors = [Fore.RED, Fore.GREEN, Fore.BLUE,
               Fore.YELLOW, Fore.MAGENTA, Fore.CYAN]
+    stepmotor_coeff = 100
+    distances = {0: 1, 1: 1, 2: 1, 3: 1, 4: 1, 5: 1,
+                 6: 1, 7: 1, 8: 1, 9: 1, 10: 1, 11: 1}
     def __init__(self):
         self.id = Arm.getId()
         super().__init__(name = self.id)
@@ -19,20 +22,39 @@ class Arm(Thread):
         self.IMPULSES_PER_DISTANCE = 1
         self.pprint('Initialized arm {}')
 
+    #Utility, for testing use!------------------------------#
+    def getId():
+        Arm.id+=1
+        return Arm.id
+
     def pprint(self, string):
         print(self.color + string.format(self.id) + '\r',
               flush=True)
         #print(string.format(self.id))
+    #-------------------------------------------------------#
+
+    #Timing based functions---------------------------------#
+    def nextTic(self):
+        self.strum()
+        self.increaseTic()
+
+    def strum(self):
+        self.pprint("strum string {}")
 
     def increaseTic(self):
         self.tic += 1
-        
+    #-------------------------------------------------------#
+
+    #Run sequence, in order of execution--------------------#
     def setNotes(self, notes):
         self.notes = notes
         self.ready = True
 
+    def run(self):
+        self.pprint('Running arm {}')
+        self.initMovement()
+    
     def initMovement(self):
-        #import pdb;pdb.set_trace()
         if self.ready:
             self.pprint(str(self.notes))
             for note in self.notes:
@@ -41,25 +63,19 @@ class Arm(Thread):
                 while note[1] >= self.tic:
                     sleep(0.1)
         else:
-            self.pprint("No notes available")
+            self.pprint("No notes available to play!")
 
     def moveTo(self, destination):
         delta = abs(self.pos - destination)
         impulses = delta * self.IMPULSES_PER_DISTANCE
         self.pprint('Arm {}: '+'{} impulses'.format(impulses))
         self.pos = destination
+    #-------------------------------------------------------#
 
-    def getId():
-        Arm.id+=1
-        return Arm.id
-
-    def run(self):
-        self.pprint('Running arm {}')
-        self.initMovement()
-        
 class Supervisor:
     def __init__(self):
         self.arms = []
+        self.tic_time = 1
 
     def addArms(self, arms):
         self.arms += arms
@@ -75,10 +91,10 @@ class Supervisor:
         sleep(1)
         while tic < 13:
             for arm in self.arms:
-                arm.increaseTic()
+                arm.nextTic()
             print("tic " + str(tic))
             tic += 1
-            sleep(1)
+            sleep(self.tic_time)
 
     def genRan(self):
         tuples = []
@@ -109,7 +125,7 @@ if __name__ == "__main__":
     supervisor = Supervisor()
 
     arms = []
-    for num in range(6):
+    for num in range(2):
         current_arm = Arm()
         current_arm.setNotes(supervisor.genRan())
         arms += [current_arm]
